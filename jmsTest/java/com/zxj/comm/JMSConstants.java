326a443338c748ecf6fb597086f5e8f1568afa49
zhang4838223 <409751089@qq.com>
2016-07-13 14:29:48 +0800

    生产者根据配置文件参数，生成指定队列并发送数据（根据取余数）

--- jmsTest/java/com/zxj/comm/JMSConstants.java
+++ jmsTest/java/com/zxj/comm/JMSConstants.java
@@ -1,10 +1,28 @@
 package com.zxj.comm;
 
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * Created by zhang4838223 on 2016/7/9.
  */
 public class JMSConstants {
 
     /** 每BATCH_SIZE条数据发送*/
-    public static final int BATCH_SIZE = 5000;
+    public static final int BATCH_SIZE = 10000;
+
+    public static int queuqSize = 1;
+
+    public static int getQueueSize(){
+        try {
+            Properties prop = PropertiesLoaderUtils.loadAllProperties("prop.properties");
+            String size = prop.getProperty("queueSize", "1");
+            queuqSize = Integer.valueOf(size);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return queuqSize;
+    }
 }
